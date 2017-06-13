bf58ded95180ae65a54ab48da9da524b26c85a03
kho <kho@tams.com>
2017-06-12 17:25:33 -0700

    copied from prod project

---
+++ Scripts/environment-configs.js
@@ -0,0 +1,29 @@
+//This is a configuration file for all hard-coded values in a specific environment
+
+//This AppVersion must be matched with the version in API.
+//Otherwise, the app will force users to update
+var AppVersion = "5.4.14";
+
+var Configs = {
+
+    //"ServiceRootUrl": "https://intranet.tams.com/VirtualApps/BUSOPSWebs/ULAppsService/",
+    //"SharePointRootUrl": "https://intranet.tams.com/",
+    //"EquipmentSitePath": "sites/busops/Logistics/DER",
+    //"SitePath": "sites/busops",
+    //"MKTSitePath": "sites/marketing"
+
+	
+	
+	"ServiceRootUrl": "http://tusspdev1wfe4/VirtualApps/TAMS.BUSOPS/TAMS.BUSOPS.DemoESR.MobileSVC/",
+	"SharePointRootUrl" : "http://tusspdev1wfe4/",
+	"EquipmentSitePath": "sites/busops/Logistics/DER",
+	"SitePath": "sites/busops",
+    "MKTSitePath": "sites/marketing"		
+
+    //"ServiceRootUrl": "https://intranetqa.tams.com/VirtualApps/TAMS.BUSOPS/BUSOPSWebs/TAMS.BUSOPS.DemoESR.MobileSVC/",
+    //"SharePointRootUrl": "https://intranetqa.tams.com/",
+    //"EquipmentSitePath": "sites/busops/Logistics/DER",
+    //"SitePath": "sites/busops",
+    //"MKTSitePath": "sites/marketing"
+	
+};
